ab79433bd4f06986c3a5e23d4765e49877ff19c6
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-10-18 17:42:59 +0200

    avoid having issues with files not being loaded correctly by explicitly requiring them

--- lib/crm_friendly.rb
+++ lib/crm_friendly.rb
@@ -1,6 +1,6 @@
 %w(
-error_proxy.rb
-friendly_extensions.rb
-).each { |libname| require libname }
+  error_proxy
+  friendly_extensions
+).each { |a| require File.dirname(__FILE__) + "/#{a}.rb" }
 
 class Friendly::HasIndex < Friendly::Error; end
